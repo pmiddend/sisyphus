fc64037111d6ab41ec1d1a875003034125af4397
Philipp Middendorf <philipp.middendorf@desy.de>
2022-12-21 10:02:23 +0100

    ✨ Prepare closurecompiler optimization pass

--- default.nix
+++ default.nix
@@ -1,9 +1,23 @@
-with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/refs/tags/1.8.tar.gz";
-}) {});
-{
+with (import
+  (builtins.fetchTarball {
+    url = "https://github.com/dmjio/miso/archive/refs/tags/1.8.tar.gz";
+  })
+{ });
+let
   dev = pkgs.haskell.packages.ghc865.callCabal2nix "app" ./. { miso = miso-jsaddle; };
-  release = pkgs.haskell.packages.ghcjs86.callCabal2nix "app" ./. {};
+  client = pkgs.haskell.packages.ghcjs86.callCabal2nix "app" ./. { };
+in
+{
+  inherit dev;
+  release = client;
+  isomorphic = pkgs.runCommand "sisyphus.org" { inherit client; } ''
+    mkdir -p $out
+    ${pkgs.closurecompiler}/bin/closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS \
+      --jscomp_off=checkVars \
+      --externs=${client}/bin/app.jsexe/all.js.externs \
+      ${client}/bin/app.jsexe/all.js > temp.js
+    mv temp.js $out/all.js
+  '';
   inherit pkgs;
 }
 
